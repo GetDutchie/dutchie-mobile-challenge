8ed4584066b320bb02a349ba874d0237328b56f9
Brandon Long <37224947+devbrandonlong@users.noreply.github.com>
2021-06-21 23:34:59 -0400

    ENG-3604: Remove extra api files

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 api: PORT=5000 yarn server
-web: yarn dev
--- pages/the-result.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import PageWrapper from 'components/layout/page-wrapper';
-
-export default function TheResultPage() {
-  return <PageWrapper heading='The Result' icon='menu'></PageWrapper>;
-}
