29b6fb69f839956c71f98705461a84b7583d228b
Jacob Sheehy <jacob.sheehy@gmail.com>
2026-05-11 03:40:13 -0700

    adding a simple test for song duration

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,5 +1,13 @@
+import 'package:app/models/music.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('MusicApp', () {});
+  test('durationLabel works as expected', () {
+    const song = Song(
+      id: 'song-1',
+      title: 'Track 1',
+      durationMs: 125000,
+    );
+    expect(song.durationLabel, '2:05');
+  });
 }
